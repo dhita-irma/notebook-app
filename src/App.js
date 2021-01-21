85cced34cdb3acb9836fa2a0c15da7630cd8667f
Dhita Irma <dhita.irma@gmail.com>
2021-01-21 16:30:11 +0800

    Setup layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button, Container, Row, Col } from 'react-bootstrap';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        {/* <h1>Notebook</h1> */}
       </header>
+      <Container className="note-container">
+        <Row>
+          <Col md={12} className="note-header">NOTES</Col>
+        </Row>
+        <Row>
+          <Col md={4} className="sidebar">
+            <Button variant="outline-warning">Create Note</Button>
+          </Col>
+          <Col md={8} className="main">
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
