e1d1742f860a82203d023ed3b26686df404ea948
Dhita Irma <dhita.irma@gmail.com>
2021-01-24 23:59:05 +0800

    Add id to note list

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, showNoteHandler }) => {
   return(
     <div className="notelist-container text-left">
       <ul className="note-list">
-        {notes.map((item, index) => 
-          <li key={index}>{item.title}</li>)}
+        {notes.map((item) => 
+          <li className="note-list-item" key={item.id} id={item.id} onClick={showNoteHandler}>
+            {item.title}
+          </li>)}
       </ul>
     </div>
   );
