8e1fe12bb93c80ea243f7df0baafe87819ab7f7b
Dhita Irma <dhita.irma@gmail.com>
2021-01-26 21:34:18 +0800

    Update NoteItem.js

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import {React, Fragment} from 'react';
 import { Button } from 'react-bootstrap';
 
 
 const NoteItem = ({ noteShowed, deleteNoteHandler }) => {
-    if (noteShowed.length === 0) {
-        return (
-            <div>No notes to be shown.</div>
-        )
-    } else {
-        return(
-                <div className="note-item">
+    return (
+        <div className="note-item">
+            {noteShowed.length === 0 ? <div>No notes to be shown.</div> :
+                <Fragment>
                     <h3>{noteShowed.title}</h3>
                     <p>{noteShowed.content}</p>
                     <Button variant="warning" className="mr-3">Edit</Button>
                     <Button variant="danger" onClick={deleteNoteHandler}>Delete</Button>
-                </div> 
-        );
-    }
+                </Fragment>
+            }
+        </div>
+    );
 }
 
 export default NoteItem;
